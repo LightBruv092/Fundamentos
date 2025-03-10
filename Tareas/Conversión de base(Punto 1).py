e12ac31d834a5f1314076ce074521b4d5c94e25f
lightBruv092 <davidlizarazof@gmail.com>
2025-03-09 20:46:10 -0500

    Tareas

---
+++ Tareas/Conversión de base(Punto 1).py
@@ -0,0 +1,15 @@
+N=int(input("Ingrese el número: "))
+B=int(input("Ingrese la base a convertir: "))
+D=2<=B<10
+V=""
+if N!=0:
+    if D == True:
+        while N>0:
+            R=N%B
+            V=str(R)+V
+            N//=B
+        print(V)
+    else:
+        print("Base fuera del rango")
+else:
+    print(0)
